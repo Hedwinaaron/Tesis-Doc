1d0feb5c71f6b9b803f26a63381b6798ce67df71
Hedwin Aaron Encinas Acosta <hedwinaarona@gmail.com>
2022-03-23 10:03:36 +0100

    update

--- openroot.py
+++ openroot.py
@@ -28,9 +28,12 @@ Entries=tree.GetEntries()
 #h1create and fill the histogram
 can  = ROOT.TCanvas("can", "histograms", 1400,700)
 can2  = ROOT.TCanvas("can2", "histogramss", 1400,700)
+can3  = ROOT.TCanvas("can3", "Resolution", 1400,700)
 h1 = ROOT.TH1D("h1", "Down dGl", 700, -200.0, 500.0)
 h2 = ROOT.TH1D("h2", "Up dGl", 700, -200.0, 500.0)
 updgl_pt=np.empty(52, dtype = float)
+up=[]
+down=[]
 for i in range(0,Entries):
     tree.GetEntry(i) 
     pair_dphi= abs(tree.tag_phi-tree.probe_updgl_phi)
@@ -44,22 +47,17 @@ for i in range(0,Entries):
             downdgl_pt = tree.tag_pt
             print("Down dGl",downdgl_pt)
             h1.Fill(downdgl_pt)
+            down.append(1/downdgl_pt)
         
         if tree.probe_isUpdGlobal == 1 and tree.probe_updgl_pt > 20 and pair_dphi_reduced > 2.8:
             updgl_pt = tree.tag_pt
             h2.Fill(updgl_pt)    
             print("Up dGl",updgl_pt)
+            up.append(1/updgl_pt)
             
             
             
             
-            
-
-        
-
-
-
-
 can.cd()
 h1.Draw()
 can.SaveAs("downdgl_pt.png")
@@ -67,4 +65,22 @@ can.SaveAs("downdgl_pt.png")
 can2.cd()    
 h2.Draw()
 can2.SaveAs("updgl_pt.png")
+
+up_dGl_pt=np.array(up)
+down_ndGl_pt=np.array(down)
+n=50
+x=np.empty(n)
+y=np.empty(n)
+for i in range(0,n):
+    x[i]=up_dGl_pt[i]
+    y[i]=(up_dGl_pt[i]-down_ndGl_pt[i])/(np.sqrt(2)*down_ndGl_pt[i])
+gr = ROOT.TGraph( n, x, y )
+gr.SetTitle( 'Pt Resolution' )
+gr.GetXaxis().SetTitle( '1/pt' )
+gr.GetYaxis().SetTitle( 'Resolution' )
+can3.cd()
+gr.Draw('PA*')
+can3.SaveAs("Resolution.png")
+       
+        
 
