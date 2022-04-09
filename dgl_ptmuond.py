67cde3c03f51871f8b7d9459e3074ca6392d9cc8
Hedwin Aaron Encinas Acosta <hedwinaarona@gmail.com>
2022-04-09 02:42:59 +0200

    update

--- dgl_ptmuond.py
+++ dgl_ptmuond.py
@@ -11,231 +11,127 @@ inFileName = sys.argv[1]
 
 #Open root file and get branch
 inFile = ROOT.TFile.Open( inFileName ," READ ")
-tree = inFile.Get ("muon/Events")
 print "Reading from ", inFileName      
-Entries=tree.GetEntries()
 
-
-#h1create and fill the histogram
 can  = ROOT.TCanvas("can", "histograms", 1400,700)
-can2  = ROOT.TCanvas("can2", "histogramss", 1400,700)
-can3  = ROOT.TCanvas("can3", "Average Resolution", 1400,700)
-can4 = ROOT.TCanvas("can4", "Resolution", 1400,700)
-can5 = ROOT.TCanvas("can5", "Resolution hist", 1400,700)
-can6= ROOT.TCanvas("can6", "Pt hist", 1400,700)
-can7= ROOT.TCanvas("can7", "Pt hist", 1400,700)
-can8= ROOT.TCanvas("can8", "Pt hist", 1400,700)
-can9= ROOT.TCanvas("can9", "Pt hist", 1400,700)
-can10= ROOT.TCanvas("can10", "Pt hist", 1400,700)
-can11= ROOT.TCanvas("can11", "Sigma", 1400,700)
-h1 = ROOT.TH1D("h1", "Down dGl", 500, 0.0, 500.0)
-h2 = ROOT.TH1D("h2", "Up dGl", 500, 0.0, 500.0)
-h3= ROOT.TH1D("h3", "Resolution hist", 200, -1, 1)
-h4= ROOT.TH1D("h4", "Res Pt_0-100 hist ", 100, -0.1, 0.1)
-h5= ROOT.TH1D("h5", "Res Pt_100-200 hist ", 100, -0.1, 0.1)
-h6= ROOT.TH1D("h6", "Res Pt_200-300 hist ", 100, -0.1, 0.1)
-h7= ROOT.TH1D("h7", "Res Pt_300-400 hist ", 100, -0.1, 0.1)
-h8= ROOT.TH1D("h8", "Res Pt_400-500 hist ", 100, -0.1, 0.1)
+can2  = ROOT.TCanvas("can2", "histograms", 1400,700)
 
-entry_down=0
-entry_up=0
-downdgl_pt=0
-updgl_pt=0
-k=-1
-n=5
-inv_Uppt=0
-inv_Downpt=0
-res=0
-xaxis=[]
-ressolution=[]
-den=np.array([0,0,0,0,0])
-empty=np.array([0.0,0.0,0.0,0.0,0.0])
-sum_resolution=np.array([0.0,0.0,0.0,0.0,0.0])
-sigma=np.array([0.0,0.0,0.0,0.0,0.0])
-average_resolution=np.array([0.0,0.0,0.0,0.0,0.0])
-average_resolution1=np.array([0.0,0.0,0.0,0.0,0.0])
-res_error=np.array([0.0,0.0,0.0,0.0,0.0])
-sigma_error=np.array([0.0,0.0,0.0,0.0,0.0])
-pt_bin=np.array([50.0,150.0,250.0,350.0,450.0])
 
-for i in range(0,Entries):
-    tree.GetEntry(i) 
-    pair_dphi= abs(tree.tag_phi-tree.probe_updgl_phi)
-    pair_dphi_reduced= abs(pair_dphi - 3.141592 * (1 + (pair_dphi-3.141592)/abs(pair_dphi-3.141592)))
-    tag_abseta= abs(tree.tag_eta)
-    if tree.HLT_L2Mu10_NoVertex_NoBPTX3BX_v == 1 and tree.tag_pt > 20 and tag_abseta < 0.9: 
-        
-        if tree.tag_phi < -0.6 and tree.tag_phi > -2.6 and tree.probe_downdgl_pt >20 :
-            entry_down=i
-            downdgl_pt = tree.probe_downdgl_pt
-        
-            
-    
-        if  tree.probe_isUpdGlobal == 1 and tree.probe_updgl_pt > 20 and pair_dphi_reduced > 2.8:
-            entry_up=i
-            updgl_pt = tree.probe_updgl_pt
-            
-               
-                
-        if entry_up==entry_down:
-            if updgl_pt==0 or downdgl_pt==0:
-                continue
-            print("Event", i)
-            h1.Fill(downdgl_pt)
-            print("Down dGl",downdgl_pt)
-            h2.Fill(updgl_pt)
-            print("Up dGl",updgl_pt)
-            
-            inv_Uppt=1/updgl_pt
-            inv_Downpt=1/downdgl_pt
-            res=((inv_Uppt)-(inv_Downpt))/(np.sqrt(2)*(inv_Downpt))
-            h3.Fill(res)
-            k+=1
-            print(k,inv_Uppt,inv_Downpt)
-            xaxis.append(inv_Uppt)
-            ressolution.append(res)
-            
-            if 0.0<updgl_pt<pt_bin[0]+50.0:
-                den[0]+=1
-                sum_resolution[0]+=res
-                h4.Fill(res)
-            
-            if (pt_bin[1]-50.0)<updgl_pt<(pt_bin[1]+50.0):
-                den[1]+=1
-                sum_resolution[1]+=res
-                h5.Fill(res)
-                
-            if (pt_bin[2]-50.0)<updgl_pt<(pt_bin[2]+50.0):
-                den[2]+=1
-                sum_resolution[2]+=res
-                h6.Fill(res)
-                
-            if (pt_bin[3]-50.0)<updgl_pt<(pt_bin[3]+50.0):
-                den[3]+=1
-                sum_resolution[3]+=res
-                h7.Fill(res)
-            
-            if (pt_bin[4]-50.0)<updgl_pt<(pt_bin[4]+50.0):
-                den[4]+=1
-                sum_resolution[4]+=res
-                h8.Fill(res)
-                
-                
-for s in range(5):
-    average_resolution1[s]= abs(sum_resolution[s])/den[s]
-    
-            
-            
-can.cd()
-h1.Draw()
-can.SaveAs("plots/do_ndgl_pt.png")
+#Relevan variables
+n=8
+sigma=np.array([0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0])
+average_resolution=np.array([0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0])
+res_error=np.array([0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0])
+sigma_error=np.array([0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0])
+empty=np.array([0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0])
 
-can2.cd()    
-h2.Draw()
-can2.SaveAs("plots/up_dgl_pt.png")
+#Bining
+pt_bin=np.array([35.0,45.0,60.0,85.0,125.0,175.0,275.0,425.0])
 
-can5.cd()    
-h3.Draw()
-can5.SaveAs("plots/Resolutions_hist.png")
 
-can6.cd()
-fit1= h4.Fit("gaus", "S")
+#Fitting histograms and extracting mean values, sigma anc correspondng errors
+h1=inFile.Get("pt_30-40_hist")
+fit1= h1.Fit("gaus", "S")
 mean_res1=fit1.Parameter(1)
 sigma1=fit1.Parameter(2)
 res_error[0]=fit1.ParError(1)
 sigma_error[0]=fit1.ParError(2)
-print("1",mean_res1,sigma1)
 average_resolution[0]=mean_res1
 sigma[0]=sigma1
-h4.Draw()
-can6.SaveAs("plots/pt_0-100_hist.png")
 
-can7.cd()
-fit2= h5.Fit("gaus", "S")
+
+
+h2=inFile.Get("pt_40-50_hist")
+fit2= h2.Fit("gaus", "S")
 mean_res2=fit2.Parameter(1)
 sigma2=fit2.Parameter(2)
 res_error[1]=fit2.ParError(1)
 sigma_error[1]=fit2.ParError(2)
-print("2",mean_res2, sigma2)
 average_resolution[1]=mean_res2
 sigma[1]=sigma2
-h5.Draw()
-can7.SaveAs("plots/pt_100-200_hist.png")
 
-can8.cd()
-fit3= h6.Fit("gaus", "S")
+h3=inFile.Get("pt_50-70_hist")
+fit3= h3.Fit("gaus", "S")
 mean_res3=fit3.Parameter(1)
 sigma3=fit3.Parameter(2)
 res_error[2]=fit3.ParError(1)
 sigma_error[2]=fit3.ParError(2)
-print("3",mean_res3, sigma3)
 average_resolution[2]=mean_res3
 sigma[2]=sigma3
-h6.Draw()
-can8.SaveAs("plots/pt_200-300_hist.png")
 
-can9.cd()
-fit4= h7.Fit("gaus", "S")
+
+h4=inFile.Get("pt_70-100_hist")
+fit4= h4.Fit("gaus", "S")
 mean_res4=fit4.Parameter(1)
 sigma4=fit4.Parameter(2)
 res_error[3]=fit4.ParError(1)
 sigma_error[3]=fit4.ParError(2)
-print("4",mean_res4, sigma4)
 average_resolution[3]=mean_res4
 sigma[3]=sigma4
-h7.Draw()
-can9.SaveAs("plots/pt_300-400_hist.png")
 
-can10.cd()
-fit5= h8.Fit("gaus", "S")
+h5=inFile.Get("pt_100-150_hist")
+fit5= h5.Fit("gaus", "S")
 mean_res5=fit5.Parameter(1)
 sigma5=fit5.Parameter(2)
 res_error[4]=fit5.ParError(1)
 sigma_error[4]=fit5.ParError(2)
-print("5",mean_res5, sigma5,res_error,sigma_error)
 average_resolution[4]=mean_res5
 sigma[4]=sigma5
-h8.Draw()
-can10.SaveAs("plots/pt_400-500_hist.png")
-print(average_resolution)
 
 
-MU=ROOT.TMultiGraph()
-gu = ROOT.TGraph( n, pt_bin, average_resolution1, )
-gu.SetMarkerColor( 4 )
-MU.Add(gu)
+h6=inFile.Get("pt_150-200_hist")
+fit6= h6.Fit("gaus", "S")
+mean_res6=fit6.Parameter(1)
+sigma6=fit6.Parameter(2)
+res_error[5]=fit6.ParError(1)
+sigma_error[5]=fit6.ParError(2)
+average_resolution[5]=mean_res6
+sigma[5]=sigma6
+
+
+h7=inFile.Get("pt_200-350_hist")
+fit7= h7.Fit("gaus", "S")
+mean_res7=fit7.Parameter(1)
+sigma7=fit7.Parameter(2)
+res_error[6]=fit7.ParError(1)
+sigma_error[6]=fit7.ParError(2)
+average_resolution[6]=mean_res7
+sigma[6]=sigma7
+
+
+h8=inFile.Get("pt_350-500_hist")
+fit8= h8.Fit("gaus", "S")
+mean_res8=fit8.Parameter(1)
+sigma8=fit8.Parameter(2)
+res_error[7]=fit8.ParError(1)
+sigma_error[7]=fit8.ParError(2)
+average_resolution[7]=mean_res8
+sigma[7]=sigma8
+
+
+#Creating and saving resolutions graphs
 gr = ROOT.TGraphErrors( n, pt_bin, average_resolution,empty,res_error )
-MU.Add(gr)
-MU.SetTitle( 'Pt Resolution' )
-MU.GetXaxis().SetTitle( 'pt' )
-MU.GetYaxis().SetTitle( 'Average Resolution' )
-MU.GetYaxis().SetRangeUser( -0.3,0.1 )
-legend = ROOT.TLegend(.73,.92,.97,.73);
-legend.AddEntry(gu,"Average resolution","lep");
-legend.AddEntry(gr," Gaus Fit mean","lep");
-can3.cd()
-MU.Draw('PA*')
-legend.Draw()
-can3.SaveAs("plots/Average_Resolution.png")
+gr.SetTitle( 'Pt Resolution' )
+gr.GetXaxis().SetTitle( 'pt' )
+gr.GetYaxis().SetTitle( 'Average Resolution' )
+gr.GetYaxis().SetRangeUser( -0.06,0.06 )
+can.cd()
+gr.Draw('PA*')
+can.SaveAs("plots/Average_Resolution.png")
 
-
-x=np.array(xaxis)
-y=np.array(ressolution)
-gp = ROOT.TGraph( k, x, y )
-gp.SetTitle( 'Pt Resolution' )
-gp.GetXaxis().SetTitle( '1/pt' )
-gp.GetYaxis().SetTitle( 'Resolution' )
-gp.GetYaxis().SetRangeUser( -0.1,0.1 )
-can4.cd()
-gp.Draw('PA*')
-can4.SaveAs("plots/Resolution.png")
 
 gs = ROOT.TGraphErrors( n, pt_bin, sigma,empty,sigma_error )
 gs.SetTitle( 'Sigma' )
 gs.GetXaxis().SetTitle( 'pt' )
 gs.GetYaxis().SetTitle( 'Standar deviation' )
-gs.GetYaxis().SetRangeUser( 0.0,0.3 )
-can11.cd()
+gs.GetYaxis().SetRangeUser( 0.0,0.1 )
+can2.cd()
 gs.Draw('PA*')
-can11.SaveAs("plots/Sigma.png")
+can2.SaveAs("plots/Sigma.png")
+
+
+
+
+
+
     
--- openroot.py
+++ openroot.py
@@ -2,13 +2,6 @@ import ROOT
 import numpy as np
 import array as arr
 import sys
-def histogram(htitle, nbins, lowxlim, maxxlim, ent,values):
-    c1  = ROOT.TCanvas("c1", "histogram", 1400,700)
-    h1 = ROOT.TH1D("h1", htitle, int(nbins), int(lowxlim), int(maxxlim))
-    for i in range(0,int(ent)):
-        h1.Fill(values)
-    h1.Draw()
-    c1.SaveAs("histogram.png")
 
 if len( sys.argv ) != 2:
   print"USAGE: %s <input file >"%( sys.argv [0])
@@ -20,67 +13,134 @@ inFileName = sys.argv[1]
 inFile = ROOT.TFile.Open( inFileName ," READ ")
 tree = inFile.Get ("muon/Events")
 print "Reading from ", inFileName      
-#Branch=tree.GetBranch("run")
-#Branch=tree.GetBranch("probe_pt")
 Entries=tree.GetEntries()
 
 
-#h1create and fill the histogram
-can  = ROOT.TCanvas("can", "histograms", 1400,700)
-can2  = ROOT.TCanvas("can2", "histogramss", 1400,700)
-can3  = ROOT.TCanvas("can3", "Resolution", 1400,700)
-h1 = ROOT.TH1D("h1", "Down dGl", 700, -200.0, 500.0)
-h2 = ROOT.TH1D("h2", "Up dGl", 700, -200.0, 500.0)
-updgl_pt=np.empty(52, dtype = float)
-up=[]
-down=[]
+#create and fill the histogram
+h1 = ROOT.TH1D("h1", "Down dGl", 500, 0.0, 500.0)
+h2 = ROOT.TH1D("h2", "Up dGl", 500, 0.0, 500.0)
+h3= ROOT.TH1D("h3", "Resolution hist", 200, -1, 1)
+h4= ROOT.TH1D("h4", "Res Pt_30-40 hist ", 100, -0.1, 0.1)
+h5= ROOT.TH1D("h5", "Res Pt_40-50 hist ", 100, -0.1, 0.1)
+h6= ROOT.TH1D("h6", "Res Pt_50-70 hist ", 100, -0.1, 0.1)
+h7= ROOT.TH1D("h7", "Res Pt_70-100 hist ", 100, -0.1, 0.1)
+h8= ROOT.TH1D("h8", "Res Pt_100-150 hist ", 100, -0.1, 0.1)
+h9= ROOT.TH1D("h9", "Res Pt_150-200 hist ", 100, -0.1, 0.1)
+h10= ROOT.TH1D("h10", "Res Pt_200-350 hist ", 100, -0.1, 0.1)
+h11= ROOT.TH1D("h11", "Res Pt_350-500 hist ", 100, -0.1, 0.1)
+
+#relevant variables
+entry_down=0
+entry_up=0
+downdgl_pt=0
+updgl_pt=0
+k=-1
+inv_Uppt=0
+inv_Downpt=0
+res=0
+pt_bin=np.array([30,40,50,70,100,150,200,350,500])
+
+
+#Events selection and histogram filling
 for i in range(0,Entries):
     tree.GetEntry(i) 
     pair_dphi= abs(tree.tag_phi-tree.probe_updgl_phi)
     pair_dphi_reduced= abs(pair_dphi - 3.141592 * (1 + (pair_dphi-3.141592)/abs(pair_dphi-3.141592)))
+    tag_abseta= abs(tree.tag_eta)
+    if tree.HLT_L2Mu10_NoVertex_NoBPTX3BX_v == 1 and tree.tag_pt > 20 and tag_abseta < 0.9: 
+        
+        if tree.tag_phi < -0.6 and tree.tag_phi > -2.6 and tree.probe_downdgl_pt >20 :
+            entry_down=i
+            downdgl_pt = tree.probe_downdgl_pt
+        
+            
     
-    #event slectrion criteria 
-    if tree.HLT_L2Mu10_NoVertex_NoBPTX3BX_v == 1 and tree.tag_pt > 20 and tree.tag_isdGlobal == 1 and tree.tag_phi < -0.6 and tree.tag_phi > -2.6 and tree.tag_eta < 0.9and tree.isMC == 1:
-        print("Event", i)
-        
-        if tree.tag_phi < -0.6 and tree.tag_phi > -2.6:
-            downdgl_pt = tree.tag_pt
-            print("Down dGl",downdgl_pt)
+        if  tree.probe_isUpdGlobal == 1 and tree.probe_updgl_pt > 20 and pair_dphi_reduced > 2.8:
+            entry_up=i
+            updgl_pt = tree.probe_updgl_pt
+            
+               
+                
+        if entry_up==entry_down:
+            if updgl_pt==0 or downdgl_pt==0:
+                continue
+            print("Event", i)
             h1.Fill(downdgl_pt)
-            down.append(1/downdgl_pt)
-        
-        if tree.probe_isUpdGlobal == 1 and tree.probe_updgl_pt > 20 and pair_dphi_reduced > 2.8:
-            updgl_pt = tree.tag_pt
-            h2.Fill(updgl_pt)    
+            print("Down dGl",downdgl_pt)
+            h2.Fill(updgl_pt)
             print("Up dGl",updgl_pt)
-            up.append(1/updgl_pt)
             
+            inv_Uppt=1/updgl_pt
+            inv_Downpt=1/downdgl_pt
+            res=((inv_Uppt)-(inv_Downpt))/(np.sqrt(2)*(inv_Downpt))
+            h3.Fill(res)
+            k+=1
+            print(k,inv_Uppt,inv_Downpt)
             
+            if pt_bin[0]<updgl_pt<pt_bin[1]:
+                h4.Fill(res)
             
-            
-can.cd()
-h1.Draw()
-can.SaveAs("downdgl_pt.png")
+            if pt_bin[1]<updgl_pt<pt_bin[2]:
 
-can2.cd()    
-h2.Draw()
-can2.SaveAs("updgl_pt.png")
+                h5.Fill(res)
+                
+            if pt_bin[2]<updgl_pt<pt_bin[3]:
 
-up_dGl_pt=np.array(up)
-down_ndGl_pt=np.array(down)
-n=50
-x=np.empty(n)
-y=np.empty(n)
-for i in range(0,n):
-    x[i]=up_dGl_pt[i]
-    y[i]=(up_dGl_pt[i]-down_ndGl_pt[i])/(np.sqrt(2)*down_ndGl_pt[i])
-gr = ROOT.TGraph( n, x, y )
-gr.SetTitle( 'Pt Resolution' )
-gr.GetXaxis().SetTitle( '1/pt' )
-gr.GetYaxis().SetTitle( 'Resolution' )
-can3.cd()
-gr.Draw('PA*')
-can3.SaveAs("Resolution.png")
-       
-        
+                h6.Fill(res)
+                
+            if pt_bin[3]<updgl_pt<pt_bin[4]:
+
+                h7.Fill(res)
+            
+            if pt_bin[4]<updgl_pt<pt_bin[5]:
+
+                h8.Fill(res)
+            
+            if pt_bin[5]<updgl_pt<pt_bin[6]:
+                h9.Fill(res)
+            
+            
+            if pt_bin[6]<updgl_pt<pt_bin[7]:
+                h10.Fill(res)
+            
+            
+            if pt_bin[7]<updgl_pt<pt_bin[8]:
+                h11.Fill(res)
+                
+
+#creating output root file    
+outHistFile = ROOT.TFile.Open ( "Res_hist.root" ,"RECREATE")
+outHistFile.cd()
+
+#Hstograms pf pt distribution in upper and lower half
+h1.Write("down_ndgl_pt")
+
+h2.Write("up_dgl_pt")
+
+#histogram of resolution distribution
+h3.Write("Resolutions_hist")
+
+#Histograms of resolution distribution per pt interval 
+
+h4.Write("pt_30-40_hist")
+
+
+h5.Write("pt_40-50_hist")
+
+h6.Write("pt_50-70_hist")
+
+h7.Write("pt_70-100_hist")
+
+h8.Write("pt_100-150_hist")
+
+h9.Write("pt_150-200_hist")
+
+h10.Write("pt_200-350_hist")
+
+h11.Write("pt_350-500_hist")
+
+#closing output file
+outHistFile.Close()
+
+
 
